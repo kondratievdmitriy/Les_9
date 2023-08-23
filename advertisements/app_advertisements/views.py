5f303e76cf3101171b5762ac7bfe4b37afafed90
Дмитрий <130822046+kondratievdmitriy@users.noreply.github.com>
2023-08-23 20:10:29 +0500

    Add files via upload

---
+++ advertisements/app_advertisements/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Advertisement
+#from django.http import HttpResponse
+
+def index(request):
+    advertisements = Advertisement.objects.all()
+    context = {'advertisements' : advertisements}
+    return render(request, 'index.html', context)
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+# Create your views here.
